81b008e641123ae48bc6df4aef63682af9b7ed5c
Anastasia <KirillovaAP@yandex.ru>
2023-03-21 00:11:51 +0300

    Finished experiment with SIFT

--- BagOfVisaulWords.py
+++ BagOfVisaulWords.py
@@ -6,16 +6,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 from torch.utils.data import DataLoader, SubsetRandomSampler
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.neighbors import NearestNeighbors
 import faiss
 import cv2
 from os import listdir
 from os.path import join
-
-
-def path_to_pil_img(path):
-    return Image.open(path).convert("RGB")
+import pandas as pd
 
 
 class BagOfVisaulWords(nn.Module):
@@ -26,52 +23,81 @@ class BagOfVisaulWords(nn.Module):
         Args:
             clusters_num : int
                 The number of clusters
-            normalize_input : bool
-                If true, descriptor-wise L2 normalization is applied to input.
+            status : str
+                TBP
         """
         super().__init__()
         self.extractor = cv2.SIFT_create()
-        self.kmeans = KMeans(n_clusters = clusters_num)
-        self.neighbor = NearestNeighbors(n_neighbors = 20)
+        self.batch_size = 1000
+        self.kmeans = MiniBatchKMeans(
+            n_clusters=clusters_num, 
+            random_state=42, 
+            batch_size=self.batch_size,
+            n_init='auto'
+        )
+        self.neighbor = NearestNeighbors(n_neighbors = 5)
+
 
     def _get_features(self, image):
         keypoints, descriptors = self.extractor.detectAndCompute(image, None)
         return keypoints, descriptors
 
+
     def _build_histogram(self, descriptor_list):
-        cluster_alg = self.kmeans
-        histogram = np.zeros(len(cluster_alg.cluster_centers_))
-        cluster_result = cluster_alg.predict(descriptor_list)
+        histogram = np.zeros(len(self.kmeans.cluster_centers_))
+        cluster_result = self.kmeans.predict(descriptor_list.astype('double'))
         for i in cluster_result:
             histogram[i] += 1.0
         return histogram
 
+
     def fit(self, dataset_folder):
+        image_names = listdir(dataset_folder)
+        is_good = []
+
         descriptor_list = []
-        i = 0
-        for image_name in tqdm(listdir(dataset_folder), desc = 'Getting descriptors'):
+        for image_name in tqdm(image_names, desc = 'Getting descriptors'):
             image = cv2.imread(join(dataset_folder, image_name))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
             keypoint, descriptor = self._get_features(image)
-            descriptor_list.append(descriptor)
-            i += 1
-            if i == 10:
-                break
-        for descriptor in descriptor_list:
-            print(descriptor.shape)
-        self.kmeans.fit(descriptor_list)
+            if descriptor is not None:
+                descriptor_list.append(descriptor)
+                is_good.append(True)
+            else:
+                is_good.append(False)
+            
+            if len(descriptor_list) == self.batch_size:
+                self.kmeans = self.kmeans.partial_fit(np.vstack(descriptor_list).astype('double'))
+                print('Kmeans partially fitted')
+                descriptor_list.clear()
+        if len(descriptor_list) > 0:
+            self.kmeans = self.kmeans.partial_fit(np.vstack(descriptor_list).astype('double'))
+        
+        found_cnt = np.sum(np.array(is_good) == True)
+        print(f'I found descriptors for {found_cnt}/{len(image_names)} images in database')
+
+        with open('database_file_names_with_indexes.txt', 'w') as f:
+            f.write(f'index file_name is_good\n')
+            for i, file_name in enumerate(image_names):
+                if i >= len(is_good):
+                    break
+                f.write(f'{i} {file_name} {is_good[i]}\n')
 
         preprocessed_images = []
-        for descriptor in tqdm(descriptor_list, desc = 'Getting histograms'):
-            if (descriptor is not None):
+        for image_name in tqdm(image_names, desc = 'Getting histograms'):
+            image = cv2.imread(join(dataset_folder, image_name))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            keypoint, descriptor = self._get_features(image)
+
+            if descriptor is not None:
                 histogram = self._build_histogram(descriptor)
                 preprocessed_images.append(histogram)
-            i += 1
-            if i == 20:
-                break
         self.neighbor.fit(preprocessed_images)
 
     def predict(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         keypoint, descriptor = self._get_features(image)
         histogram = self._build_histogram(descriptor)
-        dist, result = neighbor.kneighbors([histogram])
+        dist, result = self.neighbor.kneighbors([histogram])
         return result
--- run_BagOfVisualWords.py
+++ run_BagOfVisualWords.py
@@ -4,6 +4,7 @@ import cv2
 from BagOfVisaulWords import BagOfVisaulWords
 from os import listdir
 from os.path import join
+from tqdm import tqdm
 
 dataset_dir = '/app/pitts_preparation/datasets/'
 test_dir = join(dataset_dir, 'pitts30k', 'images', 'test')
@@ -12,7 +13,12 @@ model = BagOfVisaulWords()
 model.fit(join(test_dir, 'database'))
 
 with open('predictions.txt', 'w') as f:
-    for image_name in tqdm(listdir(join(test_dir, 'queries')), desc='Testing'):
-        image = cv2.imread(join(dataset_folder, image_name))
+    image_names = listdir(join(test_dir, 'queries'))
+    with open('queries_file_names_with_indexes.txt', 'w') as f2:
+        for i, file_name in enumerate(image_names):
+            f2.write(f'{i} {file_name}\n')
+    
+    for i, image_name in tqdm(enumerate(image_names), desc='Testing'):
+        image = cv2.imread(join(test_dir, 'queries', image_name))
         result = model.predict(image)
-        print(result)
+        print(i, result, file=f)
